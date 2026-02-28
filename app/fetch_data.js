169629033189c0d54c8ff7fb7df716702f546422
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2026-02-28 11:06:02 +0530

    Update fetch_data.js

--- app/fetch_data.js
+++ app/fetch_data.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:8000'; //'https://lunar-calendar-laeb.onrender.com';
+const API_BASE = 'https://lunar-calendar-laeb.onrender.com';
 
 async function fetchForDate(isoDate) {
   setStatus('Loading data...');
